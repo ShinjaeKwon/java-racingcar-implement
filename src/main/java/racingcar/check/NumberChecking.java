32036a45889ee5e0aacc3501dca514b4c99d048f
ShinjaeKwon <sjk6437@gmail.com>
2022-03-13 23:11:17 +0900

    refactor : inline a method

--- src/main/java/racingcar/check/NumberChecking.java
+++ src/main/java/racingcar/check/NumberChecking.java
@@ -9,10 +9,7 @@ public class NumberChecking {
 	public static final int FORWARD_STANDARD = 4;
 
 	public static boolean isForward(int randomNumber) {
-		if (randomNumber >= FORWARD_STANDARD) {
-			return true;
-		}
-		return false;
+		return randomNumber >= FORWARD_STANDARD;
 	}
 
 	public static boolean isListLastIndex(ArrayList<Car> carsNamesList, int car) {
